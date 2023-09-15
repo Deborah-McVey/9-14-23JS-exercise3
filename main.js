b877b9aededf48c8f8b3b2a9d84aeeb7d83e0183
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-09-15 14:35:56 -0400

    added main.js file

---
+++ main.js
@@ -0,0 +1,30 @@
+/* Exercise #3
+Objective: Understand and Implement Asynchronous Code Using Async/Await.
+
+Instructions:
+
+Create an async function named fetchPosts that retrieves the first 10 posts from the JSONPlaceholder API.
+Handle any potential errors using a try/catch block within the async function. If an error occurs, log an appropriate error message.
+Invoke the fetchPosts function and log the results. 
+
+Starting Code: 
+async function fetchPosts() {
+	const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
+
+	// Your async/await code here
+}
+
+// Call the function to fetch posts
+fetchPosts(); */
+
+async function fetchPosts() {
+    try {
+        const url = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        const res = await url.json();
+        console.log("ASYNC/AWAIT: ", res); 
+    } catch (err) {
+        console.log("err: ", err);
+    }
+}
+
+fetchPosts();
